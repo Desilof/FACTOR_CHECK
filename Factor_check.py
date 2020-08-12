1f294559db0cdc05cd7a9cdb80dbe6db6c84f49e
Desilof <69539224+Desilof@users.noreply.github.com>
2020-08-12 12:05:12 +0100

    Add files via upload

---
+++ Factor_check.py
@@ -0,0 +1,8 @@
+def factor_check(x,y):
+    if x % y == 0:
+        print (True)
+    else:
+        print (False)
+#INPUT YOUR VALUES BELOW
+#NOTE X IS THE NUMBER, Y IS THE DIVISOR TO KNOW FACTOR OF X
+factor_check(82,3)
