942ca2b4ef723b1f7602f3936bc3c0a0f842a6ad
Desilof <69539224+Desilof@users.noreply.github.com>
2020-08-12 00:19:33 -1100

    Update Factor_check.py

--- Factor_check.py
+++ Factor_check.py
@@ -3,6 +3,11 @@ def factor_check(x,y):
         print (True)
     else:
         print (False)
+
 #INPUT YOUR VALUES BELOW
 #NOTE X IS THE NUMBER, Y IS THE DIVISOR TO KNOW FACTOR OF X
-factor_check(82,3)
+
+
+A = int(input('input x:'))
+B = int(input('input y:'))
+factor_check(A,B)
